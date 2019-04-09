2448fbd1bf667e0cc4e36d016130b8c8831b352a
Mohd <fmohd195@gmail.com>
2019-04-09 21:45:07 +0530

    CRUD with network requests in BS table

--- src/Components/UsersTable/UsersTable.js
+++ src/Components/UsersTable/UsersTable.js
@@ -5,11 +5,14 @@ import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 import BootstrapTable from 'react-bootstrap-table-next';
 import cellEditFactory from 'react-bootstrap-table2-editor';
 
+import axios from 'axios';
+
 import getData from '../../api/getData';
+import { reject } from 'q';
 
 class UsersTable extends React.Component{
 
-    state = {columns:[], users:[], columnIds:[]}
+    state = {columns:[], users:[], columnIds:[], newlyAddedUsers:[], deletedRows:[]}
 
     componentDidMount(){
         this.setUsersData();
@@ -18,7 +21,6 @@ class UsersTable extends React.Component{
     componentDidUpdate(prevProps){
         if(prevProps.data!==this.props.data){
             this.setUsersData();
-            // console.log(prevProps.data, this.props.data)
         }
     }
 
@@ -40,13 +42,67 @@ class UsersTable extends React.Component{
         })
     }
 
+    renderAddDelete = (user)=>{
+        return(
+            <>
+            <button className="btn btn-primary badge mr-2" onClick={(e)=>{this.addRow(e,user)}}>Add</button>
+            <button className="btn btn-danger badge" onClick={(e)=>{this.removeRow(e,user)}}>Delete</button>
+            </>
+        )
+    }
+
+    removeRow = (e, user)=>{
+        let usersCopy = [...this.state.users];
+        let newlyAddedFlag = false;
+        let newUsers = usersCopy.filter((item, index)=>{
+            return item.id!==user.id
+        })
+        let newlyAdded = this.state.newlyAddedUsers.filter((item,index)=>{
+            if(user.id===item.id){
+                newlyAddedFlag = true;
+            }
+            return item.id!== user.id
+        })
+
+        if(newlyAddedFlag){
+            this.setState({users:[...newUsers], newlyAddedUsers:[...newlyAdded]})
+        }
+        else{
+            this.setState({users:[...newUsers], deletedRows:[...this.state.deletedRows, user]})
+        }
+        //If newly added
+
+    }
+    
+    addRow = (e,user)=>{
+        let usersCopy = this.state.users;
+        let clickedRowIndex = null;
+        usersCopy.forEach((item, index)=>{
+            if(item.id === user.id){
+                clickedRowIndex = index;
+            }
+        })
+        let newUsers1 = usersCopy.slice(0,clickedRowIndex + 1);
+        let newUsers2 = usersCopy.slice(clickedRowIndex+1, usersCopy.length);
+        let newRow = {};
+        newRow ={...usersCopy[clickedRowIndex]}
+
+        let newRowId = prompt("Please enter a temporary ID", "000")
+        newRow = {...newRow, id:newRowId}
+        newUsers2 = [{...newRow, config:this.renderAddDelete(newRow)}, ...newUsers2];
+        
+        let finalUsers = newUsers1.concat(newUsers2);
+        this.setState({users:finalUsers});
+        this.setState({newlyAddedUsers:[...this.state.newlyAddedUsers,newRow]})
+    }
+
     setUsersData = ()=>{
         if(this.props.data.length>0)
         {
             let localUsers =[];
             this.props.data[0].myAccounts.forEach((account)=>{
                 account.myUsers.forEach((user)=>{
-                    localUsers = [...localUsers, user]
+                    localUsers = [...localUsers, {config:this.renderAddDelete(user),...user}]
                 })
             })
             this.setState({users:_.uniqBy(localUsers, "id"), columnIds:Object.keys(localUsers[0])}, 
@@ -85,10 +141,35 @@ class UsersTable extends React.Component{
         )
     }
 
+    handleDataSubmit = ()=>{
+        // this.state.newlyAddedUsers.forEach((item)=>{
+        // axios.post('http://localhost:3001/rootData/', item)
+        // .then(res=> console.log(res))
+        // .catch(err => console.log(err));
+        // })
+        let networkRequests = this.state.newlyAddedUsers.map((item)=>{
+            return new Promise((resolve, reject)=>{
+                axios.post('http://localhost:3001/rootData/', item)
+                .then(res=> resolve(res))
+                .catch(err => reject(err));
+                })
+            })
+        Promise.all(networkRequests)
+        .then(res => console.log(res))
+        .catch(res=> console.log(res))
+        
+        }
+        //modify
+        //color change
+        //tabs
+   
+
 
     render(){
+        console.log(this.state.deletedRows)
        return this.state.columns.length > 0 ? (
            <div>
+               <button className="btn btn-success ml-3 mt-3" onClick={this.handleDataSubmit}>Save</button>
                {this.renderTable()}
            </div>
        ) : (
